32b4ec77a7be283397a44035ecbf8919fc0c9ebe
moritz <fink.moritz@gmail.com>
2020-05-08 21:21:04 +0200

    Refactoring

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -12,23 +12,25 @@ import * as THREE from 'three';
 
 const Play = () => {
     useIonViewDidEnter(() => {
-        console.log('ionViewDidEnter event fired')
-        var scene = new THREE.Scene();
-        var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-        var renderer = new THREE.WebGLRenderer();
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const renderer = new THREE.WebGLRenderer();
+
         renderer.setSize( window.innerWidth, window.innerHeight );
-        //document.body.appendChild( renderer.domElement );
-        document.getElementById("webGLview").appendChild( renderer.domElement );
-        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-        var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-        var cube = new THREE.Mesh( geometry, material );
+        document.getElementById("webGLView").appendChild( renderer.domElement );
+
+        const geometry = new THREE.BoxGeometry(1, 1, 1);
+        const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+        const cube = new THREE.Mesh(geometry, material);
         scene.add( cube );
+
         camera.position.z = 5;
-        var animate = function () {
-            requestAnimationFrame( animate );
+
+        const animate = function () {
+            requestAnimationFrame(animate);
             cube.rotation.x += 0.01;
             cube.rotation.y += 0.01;
-            renderer.render( scene, camera );
+            renderer.render(scene, camera);
         };
         animate();
     });
@@ -43,7 +45,7 @@ const Play = () => {
                     <IonTitle>Play</IonTitle>
                 </IonToolbar>
             </IonHeader>
-            <IonContent id="webGLview"></IonContent>
+            <IonContent id="webGLView"></IonContent>
         </IonPage>
     );
 };
