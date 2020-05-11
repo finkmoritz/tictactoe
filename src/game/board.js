925de57580668ace23edfebddb99953c6bf62116
moritz <fink.moritz@gmail.com>
2020-05-11 21:08:34 +0200

    Show sphere on click (needs refactoring)

--- src/game/board.js
+++ src/game/board.js
@@ -15,7 +15,13 @@ import color from "../util/color";
 class Board extends React.Component {
     constructor(props) {
         super(props);
+        this.divRef = React.createRef();
         this.spheres = [];
+        this.raycaster = new THREE.Raycaster();
+        this.mouse = new THREE.Vector2();
+        this.renderer = null;
+        this.camera = null;
+        this.state = { width: window.innerWidth, height: 0.5*window.innerHeight };
     }
 
     static propTypes = {
@@ -26,6 +32,8 @@ class Board extends React.Component {
         isActive: PropTypes.bool,
         isMultiplayer: PropTypes.bool,
     };
+
+
 
     onClick = id => {
         if (this.isActive(id)) {
@@ -40,14 +48,26 @@ class Board extends React.Component {
     }
 
     componentDidMount() {
-        const size = 0.4*window.innerWidth;
+        this.renderer = new THREE.WebGLRenderer();
+
+        /*const updateDimensions = () => {
+            const divElement = document.getElementById("webGLView"+this.props.playerID);
+            const width = divElement.offsetWidth;
+            const height = divElement.offsetHeight;
+            this.renderer.setSize(width, height);
+            this.setState({ width: width, height: height });
+        };
+        window.addEventListener('resize', updateDimensions);*/
+
+        //this.setState({ width: 0.4*window.innerWidth, height: 0.4*window.innerHeight });
+        //console.log('width='+this.state.width);
 
         const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(50, 1, 0.1, 1000);
-        const renderer = new THREE.WebGLRenderer();
+        this.camera = new THREE.PerspectiveCamera(50, this.state.width/this.state.height, 0.1, 1000);
+        this.camera.position.z = 5;
 
-        renderer.setSize(size, size);
-        document.getElementById("webGLView"+this.props.playerID).appendChild(renderer.domElement);
+        this.renderer.setSize(this.state.width, this.state.height);
+        document.getElementById("webGLView"+this.props.playerID).appendChild(this.renderer.domElement);
 
         const gridHelper = new THREE.GridHelper(3, 3);
         gridHelper.geometry.rotateX(0.5*Math.PI);
@@ -62,61 +82,93 @@ class Board extends React.Component {
         }
         this.spheres[0].material.color.setHex(color.red);
 
-        camera.position.z = 5;
-
         const ambientLight = new THREE.AmbientLight(color.gray);
         scene.add(ambientLight);
 
         const directionalLight = new THREE.DirectionalLight(color.white, 0.5);
         scene.add(directionalLight);
 
+        const onClick = ( event ) => {
+            event.preventDefault();
+            this.mouse.x = ( event.clientX / this.state.width ) * 2 - 1;
+            this.mouse.y = - ( event.clientY / this.state.height ) * 2 + 1;
+        };
+        document.addEventListener( 'mousedown', onClick, false );
+        document.addEventListener( 'touchstart', onClick, false );
+
+        let INTERSECTED = null;
+
         const animate = () => {
             requestAnimationFrame(animate);
+
+            this.raycaster.setFromCamera( this.mouse, this.camera );
+            const intersects = this.raycaster.intersectObjects( scene.children );
+            if ( intersects.length > 0 ) {
+                if ( INTERSECTED !== intersects[ 0 ].object ) {
+                    this.resetPreviousIntersected(INTERSECTED);
+                    INTERSECTED = intersects[ 0 ].object;
+                    if(this.spheres.find(s => s === INTERSECTED)) {
+                        INTERSECTED.visible = true;
+                    }
+                    INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
+                }
+            } else {
+                this.resetPreviousIntersected(INTERSECTED);
+                INTERSECTED = null;
+            }
+
             for(let i=0; i<this.spheres.length; i++) {
                 switch (this.props.G.cells[i]) {
-                    case '0':
+                    case 0:
                         this.spheres[i].material.color.setHex(color.red);
                         break;
-                    case '1':
+                    case 1:
                         this.spheres[i].material.color.setHex(color.blue);
                         break;
                     default: this.spheres[i].material.color.setHex(color.gray);
                 }
             }
-            renderer.render(scene, camera);
+
+            this.renderer.render(scene, this.camera);
         }
         animate();
     };
 
+    resetPreviousIntersected(INTERSECTED) {
+        if (INTERSECTED) {
+            INTERSECTED.material.color.setHex(INTERSECTED.currentHex);
+            if (this.spheres.find(s => s === INTERSECTED)) {
+                INTERSECTED.visible = false;
+            }
+        }
+    }
+
+    /*componentDidUpdate(prevProps, prevState, snapshot) {
+            const divElement = document.getElementById("webGLView"+this.props.playerID);
+            const width = divElement.offsetWidth;
+            console.log('width='+width);
+            const height = divElement.offsetHeight;
+            if(width !== prevState.width || height !== prevState.height) {
+                console.log('state changed: '+width);
+                this.renderer.setSize(width, height);
+                this.camera.aspect = width/height;
+                this.setState({ width: width, height: height });
+            }
+        }*/
+
     createSphere(x, y, color) {
         const geometry = new THREE.SphereBufferGeometry(0.4, 32, 32);
-        const material = new THREE.MeshBasicMaterial({color: color});
+        const material = new THREE.MeshLambertMaterial({color: color});
         const sphere = new THREE.Mesh(geometry, material);
         sphere.translateX(x);
         sphere.translateY(y);
+        //sphere.translateZ(0.001);
+        sphere.visible = false;
         return sphere;
     }
 
     render() {
-        /*let tbody = [];
-        for (let i = 0; i < 3; i++) {
-            let cells = [];
-            for (let j = 0; j < 3; j++) {
-                const id = 3 * i + j;
-                cells.push(
-                    <td
-                        key={id}
-                        className={this.isActive(id) ? 'active' : ''}
-                        onClick={() => this.onClick(id)}
-                    >
-                        {this.props.G.cells[id]}
-                    </td>
-                );
-            }
-            tbody.push(<tr key={i}>{cells}</tr>);
-        }*/
-
-        let winner = null;
+        /*let winner = null;
         if (this.props.ctx.gameover) {
             winner =
                 this.props.ctx.gameover.winner !== undefined ? (
@@ -124,10 +176,13 @@ class Board extends React.Component {
                 ) : (
                     <div id="winner">Draw!</div>
                 );
-        }
+        }*/
+
+        //const { width } = this.props.size;
+        //console.log('size at render: '+width);
 
         return (
-            <div id={"webGLView"+this.props.playerID}/>
+            <div id={"webGLView" + this.props.playerID} ref={this.divRef} style={{width: '100%',}}/>
         );
     }
 }
--- src/pages/Play.js
+++ src/pages/Play.js
@@ -1,41 +1,35 @@
 import {
-    IonBackButton,
-    IonButtons, IonCol,
-    IonContent, IonGrid,
-    IonHeader,
-    IonPage, IonRow,
-    IonTitle,
-    IonToolbar, useIonViewDidEnter
+    IonCol,
+    IonContent, IonFab, IonFabButton, IonGrid, IonIcon,
+    IonPage, IonRow
 } from '@ionic/react';
 import React from 'react';
 import { Client } from "boardgame.io/react";
 import { Local } from 'boardgame.io/multiplayer';
 import TicTacToe from "../game/game";
 import Board from "../game/board";
+import {arrowBackCircle} from "ionicons/icons";
 
 const TicTacToeApp = Client({
     game: TicTacToe,
     board: Board,
     multiplayer: Local(),
-    debug: true
+    debug: false
 });
 
 const Play = () => {
 
     return (
         <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonButtons slot="start">
-                        <IonBackButton defaultHref="/home"/>
-                    </IonButtons>
-                    <IonTitle>Play</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent>
-                <IonGrid>
-                    <IonRow>
-                        <IonCol>
+            <IonContent class="ion-no-padding ion-no-margin">
+                <IonFab vertical="top" horizontal="start" slot="fixed">
+                    <IonFabButton href="/home">
+                        <IonIcon icon={arrowBackCircle} />
+                    </IonFabButton>
+                </IonFab>
+                <IonGrid class="ion-no-padding ion-no-margin">
+                    <IonRow class="ion-no-padding ion-no-margin">
+                        <IonCol class="ion-no-padding ion-no-margin">
                             <TicTacToeApp playerID="0"/>
                         </IonCol>
                         <IonCol>
